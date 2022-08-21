7aef57aab88a53d6fc0ebd59a423c502c226de0a
elpatron68 <elpatron@cepheus.uberspace.de>
2022-08-21 10:54:32 +0200

    Make CheckGithubReleses failsafe

--- SnowRunner-Tool/UpdateCheck.cs
+++ SnowRunner-Tool/UpdateCheck.cs
@@ -14,16 +14,24 @@ namespace SnowRunner_Tool
 
         public async static Task<(int, string)> CheckGithubReleses(string assemblyVersion)
         {
-            GitHubClient client = new GitHubClient(new ProductHeaderValue("SnowRunner-Tool"));
-            var releases = await client.Repository.Release.GetAll("elpatron68", "SnowRunner-Tool");
-            // (new System.Collections.Generic.Mscorlib_CollectionDebugView<Octokit.ReleaseAsset>((new System.Collections.Generic.Mscorlib_CollectionDebugView<Octokit.Release>(releases).Items[0]).Assets).Items[1]).BrowserDownloadUrl
-            string downloadUrl = releases[0].Assets[1].BrowserDownloadUrl;
-            string latest = releases[0].TagName;
-            var thisVersion = new Version(assemblyVersion);
-            var latestVersion = new Version(latest);
-            int result = latestVersion.CompareTo(thisVersion);
-            Log.Debug("Updatecheck returned {UpdateCheckResult}", result);
-            return (result, downloadUrl);
+            try
+            {
+                GitHubClient client = new GitHubClient(new ProductHeaderValue("SnowRunner-Tool"));
+                var releases = await client.Repository.Release.GetAll("elpatron68", "SnowRunner-Tool");
+                // (new System.Collections.Generic.Mscorlib_CollectionDebugView<Octokit.ReleaseAsset>((new System.Collections.Generic.Mscorlib_CollectionDebugView<Octokit.Release>(releases).Items[0]).Assets).Items[1]).BrowserDownloadUrl
+                string downloadUrl = releases[0].Assets[1].BrowserDownloadUrl;
+                string latest = releases[0].TagName;
+                var thisVersion = new Version(assemblyVersion);
+                var latestVersion = new Version(latest);
+                int result = latestVersion.CompareTo(thisVersion);
+                Log.Debug("Updatecheck returned {UpdateCheckResult}", result);
+                return (result, downloadUrl);
+            }
+            catch
+            {
+                return (0, "https://github.com/elpatron68/SnowRunner-Tool/releases/download/");
+            }
+            
         }
     }
 }
